440360aaf1aa90245639dbb21e5663008f08b748
marina saraiva <marinasaraiva@hotmail.fr>
2025-05-22 18:29:20 +0200

    feat: new dev branch

--- kids-code/src/main.tsx
+++ kids-code/src/main.tsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
         path: "/resultat",
         element: <Resultat />,
       },
-            {
+      {
 				path: "/resultats",
 				element: <ResultatBoolean />,
 			},
