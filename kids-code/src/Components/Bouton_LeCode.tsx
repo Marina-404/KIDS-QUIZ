57c9da7fcc76f19e403d195fedef0bf6490b1b6d
Maxime Giampaoli <maxime.giampaoli@gmail.com>
2025-05-22 16:06:31 +0200

    ErrorPath page responsive ok

--- kids-code/src/Components/Bouton_LeCode.tsx
+++ kids-code/src/Components/Bouton_LeCode.tsx
@@ -1,42 +1,42 @@
 import { useState } from "react";
 
 function BoutonLeCode() {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <>
-      <button
-        className="sniglet-extrabold bg-[var(--color-secondary)] text-white px-6 py-2 text-lg cursor-pointer rounded-full
+	return (
+		<>
+			<button
+				className="sniglet-regular bg-[var(--color-secondary)] text-white px-6 py-2 text-lg cursor-pointer rounded-full
 "
-        type="button"
-        onClick={() => {
-          setIsOpen(true);
-        }}
-      >
-        Le code
-        <br />
-        c'est quoi ?
-      </button>
-      {isOpen && (
-        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-          <div className="bg-white rounded-lg p-6 max-w-md text-black text-lg shadow-lg relative">
-            <button
-              className="absolute top-2 right-2 text-xl"
-              onClick={() => setIsOpen(false)}
-            >
-              ❌
-            </button>
-            <p className="sniglet-extrabold">
-              Le code informatique, c’est un peu comme une recette de cuisine,
-              mais pour les ordinateurs. On lui dit quoi faire, étape par étape
-              : afficher un message, poser une question, faire bouger un
-              personnage… C’est grâce au code qu’on peut créer des jeux, des
-              sites web, des applications, ou même des robots !
-            </p>
-          </div>
-        </div>
-      )}
-    </>
-  );
+				type="button"
+				onClick={() => {
+					setIsOpen(true);
+				}}
+			>
+				Le code
+				<br />
+				c'est quoi ?
+			</button>
+			{isOpen && (
+				<div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+					<div className="bg-white rounded-lg p-6 max-w-md text-black text-lg shadow-lg relative">
+						<button
+							className="absolute top-2 right-2 text-xl"
+							onClick={() => setIsOpen(false)}
+						>
+							❌
+						</button>
+						<p>
+							Le code informatique, c’est un peu comme une recette de cuisine,
+							mais pour les ordinateurs. On lui dit quoi faire, étape par étape
+							: afficher un message, poser une question, faire bouger un
+							personnage… C’est grâce au code qu’on peut créer des jeux, des
+							sites web, des applications, ou même des robots !
+						</p>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 export default BoutonLeCode;
--- kids-code/src/main.tsx
+++ kids-code/src/main.tsx
@@ -6,36 +6,37 @@ import App from "./App.tsx";
 import Home from "./Pages/Home";
 import Quiz from "./Pages/Quiz";
 import Resultat from "./Pages/Resultat";
+import Errorpath from "./Pages/404.tsx";
 
 const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/quiz",
-        element: <Quiz />,
-      },
-      {
-        path: "/resultat",
-        element: <Resultat />,
-      },
-      {
-        path: "*",
-        element: <h1>Tu es perdu !</h1>,
-      },
-    ],
-  },
+	{
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/quiz",
+				element: <Quiz />,
+			},
+			{
+				path: "/resultat",
+				element: <Resultat />,
+			},
+		],
+	},
+	{
+		path: "*",
+		element: <Errorpath />,
+	},
 ]);
 
 const rootElement = document.getElementById("root");
 if (rootElement == null) {
-  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
 
 if (rootElement != null) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+	ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
 }
