08788fe93fd49f521f8c7f98b02ba9aef3834f7f
MBoussel <mb.boussel@gmail.com>
2025-05-22 10:49:32 +0200

    feat: create homepage

--- kids-code/src/Pages/Home.tsx
+++ kids-code/src/Pages/Home.tsx
@@ -1,8 +1,20 @@
+import GirlPicture from "../../public/illustration.webp";
 function Home() {
   return (
     <>
       <div>
-        <h1>hello home</h1>
+        <img src={GirlPicture} alt="photo of a little girl sitting reading" />
+        <h2>Règles du quiz</h2>
+         <div className="separation" />
+         <article>
+          <p>1 - Lis bien chaque question : prend ton temps</p>
+          <p>2 - Choisi une seule bonne réponse parmi les propositions (A,B,C,D)</p>
+          <p>3 - Pas besoin d'être parfait : l'important c'est d'essayer</p>
+          <p>4 - Tu peux jouer seul ou avec des amis</p>
+          <p>5 - A la fin tu découvriras ton score</p>
+         </article>
+         <button type="button">Prêt? Go!</button>
+         <button type="button">le code c'est quoi ?</button>
       </div>
     </>
   );
